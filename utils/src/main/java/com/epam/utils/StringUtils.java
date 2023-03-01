bee16f05eb1f059731c9d38cafdfa750ce225ea4
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-03-01 20:18:27 +0500

    asf

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if(org.apache.commons.lang3.StringUtils.startsWith(str, "0")) return false;
         return NumberUtils.toDouble(str, -1) > 0;
     }
 }
